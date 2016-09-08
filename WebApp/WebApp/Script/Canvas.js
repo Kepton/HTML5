63e6f66ab2c1d185a54d9182a880eede8e68c700
liuyalongscu@foxmail.com <liuyalongscu@foxmail.com>
2016-09-08 17:15:21 +0800

    Canvas

---
+++ WebApp/WebApp/Script/Canvas.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    c = document.getElementById("myCanvas");
+    var cxt = c.getContext("2d");
+    cxt.moveTo(10, 10);
+    cxt.lineTo(150, 50);
+    cxt.lineTo(10, 50);
+    cxt.stroke();
+
+
+    var c1 = document.getElementById("Canvas1");
+    var cxt1 = c1.getContext("2d");
+    cxt1.fillStyle = "#FF0000";
+    cxt1.beginPath();
+    cxt1.arc(70, 18, 15, 0, Math.PI * 2, true);
+    cxt1.closePath();
+    cxt1.fill();
+
+    var c2 = document.getElementById("Canvas2");
+    var cxt2 = c2.getContext("2d");
+    var grd = cxt2.createLinearGradient(0, 0, 175, 50);
+    grd.addColorStop(0, "#FF0000");
+    grd.addColorStop(1, "#00FF00");
+    cxt2.fillStyle = grd;
+    cxt2.fillRect(0, 0, 175, 50);
+
+    var c3 = document.getElementById("Canvas3");
+    var cxt3 = c3.getContext("2d");
+    var img = new Image();
+    img.src = "../Resource/w3school_banner.gif";
+    cxt3.drawImage(img, 0, 0);
+}
--- WebApp/WebApp/Script/Drag.js
+++ WebApp/WebApp/Script/Drag.js
@@ -3,9 +3,16 @@
 }
 
 function drag(ev) {
+    //数据类型是 "Text"，值是可拖动元素的 id ("drag1")
     ev.dataTransfer.setData("Text", ev.target.id);
 }
 
+
+/*代码解释：
+调用 preventDefault() 来避免浏览器对数据的默认处理（drop 事件的默认行为是以链接形式打开）
+通过 dataTransfer.getData("Text") 方法获得被拖的数据。该方法将返回在 setData() 方法中设置为相同类型的任何数据。
+被拖数据是被拖元素的 id ("drag1")
+把被拖元素追加到放置元素（目标元素）中*/
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("Text");
